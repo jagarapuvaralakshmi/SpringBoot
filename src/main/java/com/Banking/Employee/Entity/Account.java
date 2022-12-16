3cf5fa40d5701e14b7fb83150687a042ceb6d3da
jagarapuvaralakshmi <97098743+jagarapuvaralakshmi@users.noreply.github.com>
2022-12-16 14:53:10 +0530

    Update Account.java
    onec

--- src/main/java/com/Banking/Employee/Entity/Account.java
+++ src/main/java/com/Banking/Employee/Entity/Account.java
@@ -13,7 +13,7 @@ import javax.persistence.ManyToOne;
 public class Account {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long accNo;
 	
 	private String accName;
